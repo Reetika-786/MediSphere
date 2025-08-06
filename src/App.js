1291bec62cdd4a85a10e13af107bc7b2790e5700
Reetika-786 <reetika848@gmail.com>
2025-08-06 17:07:08 +0530

    created structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+// import Appointments from './pages/Appointments';
+// import Doctors from './pages/Doctors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/appointments" element={<Appointments />} />
+          <Route path="/doctors" element={<Doctors />} /> */}
+          {/* Optional fallback route */}
+          <Route path="*" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
