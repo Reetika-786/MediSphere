c1d04b561863b8e44f1b02b53bee44c8164e758f
Reetika-786 <reetika848@gmail.com>
2025-12-23 20:05:31 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
-import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
@@ -11,7 +10,6 @@ import adminRouter from "./routes/adminRoute.js"
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
-connectCloudinary()
 
 // middlewares
 app.use(express.json())
